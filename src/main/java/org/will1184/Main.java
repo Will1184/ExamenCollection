16cebc067e7b63780c0c54b751f5903661b917c0
Will1184 <brandonwilliammg@gmail.com>
2023-06-11 23:12:45 -0600

    Third-Commit

--- src/main/java/org/will1184/Main.java
+++ src/main/java/org/will1184/Main.java
@@ -1,9 +1,13 @@
 package org.will1184;
 
 import org.will1184.model.Colegio;
-
+//Github: Will184
+//Autor: Brandon William Gomez Monge
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Autor: Brandon William Gomez Monge");
+        System.out.println("Github: Will184");
         Colegio c = new Colegio();
         c.addAlumno("Portuges");
         c.addAlumno("Portuges");
